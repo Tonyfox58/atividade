eb5318c8030501dda85e8a9762ecbdeffe87c7ea
Tonyfox58 <arthon58@hotmail.com>
2025-01-09 13:55:45 -0300

    Atualizar o main.c

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+//Antonio Hermogenes Ribeiro Lopes
+atividade: goto, break, continue
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,7 +11,7 @@ int main() {
     int numero, soma = 0;
 
     while (1) {
-        printf("Digite um número (-1 para sair): ");
+        printf("Digite um nÃºmero (-1 para sair): ");
         scanf("%d", &numero);
         if (numero == -1) {
             break;
@@ -15,7 +19,7 @@ int main() {
         soma += numero;
     }
 
-    printf("A soma dos números digitados é: %d\n", soma);
+    printf("A soma dos nÃºmeros digitados Ã©: %d\n", soma);
     return 0;
 }
 /*
@@ -33,14 +37,14 @@ int main() {
 int main() {
     int inicio, fim;
 
-    printf("Digite o início do intervalo: ");
+    printf("Digite o inÃ­cio do intervalo: ");
     scanf("%d", &inicio);
     printf("Digite o fim do intervalo: ");
     scanf("%d", &fim);
 
     for (int i = inicio; i <= fim; i++) {
         if (i % 7 == 0) {
-            printf("O primeiro múltiplo de 7 no intervalo é: %d\n", i);
+            printf("O primeiro mÃºltiplo de 7 no intervalo Ã©: %d\n", i);
             break;
         }
     }
@@ -53,7 +57,7 @@ int main() {
     int numero, soma = 0;
 
     for (int i = 0; i < 10; i++) {
-        printf("Digite um número: ");
+        printf("Digite um nÃºmero: ");
         scanf("%d", &numero);
         if (numero < 0) {
             continue;
@@ -61,7 +65,7 @@ int main() {
         soma += numero;
     }
 
-    printf("A soma dos números positivos é: %d\n", soma);
+    printf("A soma dos nÃºmeros positivos Ã©: %d\n", soma);
     return 0;
 }
 
@@ -72,9 +76,9 @@ int main() {
     int num1, num2;
     float resultado;
 
-    printf("Digite o primeiro número: ");
+    printf("Digite o primeiro nÃºmero: ");
     scanf("%d", &num1);
-    printf("Digite o segundo número: ");
+    printf("Digite o segundo nÃºmero: ");
     scanf("%d", &num2);
 
     if (num2 == 0) {
@@ -82,11 +86,11 @@ int main() {
     }
 
     resultado = (num1 / num2);
-    printf("O resultado da divisão é: %.2f\n", resultado);
+    printf("O resultado da divisÃ£o Ã©: %.2f\n", resultado);
 
 
      erro:
-    printf("Erro: divisão por zero não é permitida.\n");
+    printf("Erro: divisÃ£o por zero nÃ£o Ã© permitida.\n");
 
     return 0;
 }
